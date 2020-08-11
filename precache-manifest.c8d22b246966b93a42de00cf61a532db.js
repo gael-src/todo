5c781846bed3a0257a5452e2b0e31556d274be60
Gael <gaelfr85@gmail.com>
2020-08-11 12:42:36 +0200

    Updates

---
+++ precache-manifest.c8d22b246966b93a42de00cf61a532db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daca01c797108999f1e7a5c0cc9859d1",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "87f2d4080845e76782d6",
+    "url": "/todo/static/css/main.4bcf52c2.chunk.css"
+  },
+  {
+    "revision": "2b7102446546307994dc",
+    "url": "/todo/static/js/2.c881ae52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c881ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f2d4080845e76782d6",
+    "url": "/todo/static/js/main.a3a51d2e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);

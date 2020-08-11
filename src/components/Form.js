9e8c1ee9257c3966892eff50f457f7e10d3ed948
Gael <gaelfr85@gmail.com>
2020-08-11 09:44:18 +0200

    input min-max length

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ class Form extends React.Component {
 		super(props);
 		this.state = {
 			title: "",
+			correctLength: true,
 		};
 	}
 
@@ -17,25 +18,32 @@ class Form extends React.Component {
 	submitTask = (event) => {
 		event.preventDefault();
 
-		this.props.addTaskItem(this.state.title);
+		// LENGTH CHECK
+		if (this.state.title.length > 3 && this.state.title.length < 25) {
+			// SET STATE
+			this.props.addTaskItem(this.state.title);
 
-		// RESET INPUT
-		this.setState({
-			title: "",
-		});
+			// RESET INPUT
+			this.setState({
+				title: "",
+				correctLength: true,
+			});
+		} else {
+			// RESET INPUT
+			this.setState({
+				correctLength: false,
+			});
+		}
 	};
 
 	render() {
 		return (
 			<form className="task-form" action="#">
 				<label htmlFor="#">Add a task:</label>
-				<input
-					type="text"
-					value={this.state.title}
-					onChange={this.addTask}
-					minLength="3"
-					maxLength="6"
-				/>
+				<input type="text" value={this.state.title} onChange={this.addTask} />
+				{this.state.correctLength ? null : (
+					<p>Task musst be between 3 and 25 length</p>
+				)}
 				<button onClick={this.submitTask}>Submit</button>
 			</form>
 		);

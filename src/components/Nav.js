0c3bd443656a1b7666c04e76239ff85c8cd7ee91
Gael <src.gael@gmail.com>
2020-08-27 11:36:46 +0200

    saves

---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import "./css/App.scss";
+// import Header from "./components/Header";
+// import TodoContainer from "./components/TodoContainer";
+// import Footer from "./components/Footer";
+// // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// class App extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			timeToGetBusy: false,
+// 		};
+// 	}
+// 	render() {
+// 		return (
+// 			// <Router>
+// 				<div className="App">
+// 					{/* HEADER */}
+// 					<Header timeToGetBusy={this.state.timeToGetBusy} />
+
+// 					{/* MAIN TASK */}
+// 					<TodoContainer text="Text as props" />
+
+// 					{/* FOOTER */}
+// 					<Footer />
+// 				</div>
+// 			// </Router>
+// 		);
+// 	}
+// }
+
+// export default App;

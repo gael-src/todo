9b40ee5ee37d30621e682fdb7b17a0d59f53c55c
Gael <src.gael@gmail.com>
2020-08-28 10:00:28 +0200

    Updates

---
+++ precache-manifest.654373e110da4e72c5194f9f150a67fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5154c60f2bb7462bcb683f739995f0",
+    "url": "/todo-frontend/index.html"
+  },
+  {
+    "revision": "8a980d25538ca1f1ed63",
+    "url": "/todo-frontend/static/css/main.8c021a32.chunk.css"
+  },
+  {
+    "revision": "65860d137716d33d6ffc",
+    "url": "/todo-frontend/static/js/2.97b86fb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-frontend/static/js/2.97b86fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a980d25538ca1f1ed63",
+    "url": "/todo-frontend/static/js/main.7451b61c.chunk.js"
+  },
+  {
+    "revision": "3e2747a5656f07a61888",
+    "url": "/todo-frontend/static/js/runtime-main.682b5258.js"
+  },
+  {
+    "revision": "d80fbbb9bf36d4a9318c0ba8963a236c",
+    "url": "/todo-frontend/static/media/todo-illustration.d80fbbb9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-frontend/precache-manifest.ddd6156dbd3bccb498d52200dcb9a8f3.js"
+  "/todo-frontend/precache-manifest.654373e110da4e72c5194f9f150a67fb.js"
 );
 
 self.addEventListener('message', (event) => {

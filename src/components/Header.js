07a9426e1719c880e27fbf3478802314edbe5807
Gael <src.gael@gmail.com>
2020-08-28 10:21:46 +0200

    css ui update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
 import React from "react";
 import headerPics from "../assets/images/todo-illustration.jpg";
-// import { Link } from "react-router-dom";
-
 
 const Header = (props) => {
 	const h1Style = {
@@ -12,34 +10,7 @@ const Header = (props) => {
 	return (
 		<header>
 			<img className="header-pics" src={headerPics} alt="" />
-			<h1 style={h1Style}>todo</h1>
 			{props.timeToGetBusy ? <p style={h1Style}>Time to get busy!</p> : null}
-			<h2
-				style={{
-					textAlign: "center",
-					margin: "22px",
-					color: "green",
-				}}
-			>
-				React Learning to do's
-			</h2>
-			{/* <ul>
-				<Link className="nav-links" to="/">
-					<li>Home</li>
-				</Link>
-
-				<Link className="nav-links" to="/about">
-					<li>About</li>
-				</Link>
-
-				<Link className="nav-links" to="/shop">
-					<li>Shop</li>
-				</Link>
-
-				<Link className="nav-links" to="/shop/socks">
-					<li>Socks</li>
-				</Link>
-			</ul> */}
 		</header>
 	);
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 function Nav() {
 	return (
 		<nav>
-			<h3>task-src</h3>
-			<ul>
-				<li>
+			<h1 className="h1-style">task-src</h1>
+			<ul className="ul-nav">
+				<li className="li-nav">
 					<Link className="nav-links" to="/">
 						Home
 					</Link>
 				</li>
-				<li>
+				<li className="li-nav">
 					<Link className="nav-links" to="/help">
 						Help
 					</Link>
 				</li>
-				<li>
+				<li className="li-nav">
 					<Link className="nav-links" to="/Impressum">
 						Impressum
 					</Link>
